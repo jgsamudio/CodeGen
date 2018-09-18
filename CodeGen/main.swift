2a34055e9bfaff06a3268653fb2d3a7ddb3db329
Jonathan Samudio <jonathan2457@gmail.com>
2018-09-18 14:17:12 -0400

    Added xcode project to the repo.

---
+++ CodeGen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeGen
+//
+//  Created by Jonathan Samudio on 9/18/18.
+//  Copyright © 2018 Jonathan Samudio. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
